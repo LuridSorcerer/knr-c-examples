a3ac9f655cee65626230daddd93e7616558b358e
Andrew Lingenfelter <luridsorcerer@gmail.com>
2018-06-13 17:41:17 -0400

    Demonstrate input counting with a for loop (also a null statement as the loop body)

--- chapter1/count.c
+++ chapter1/count.c
@@ -7,12 +7,10 @@
 #include <stdio.h>
 
 main() {
-    long nc;
+    double nc;
 
-    nc = 0;
+    for (nc = 0; getchar() != EOF; ++nc)
+        ; /* body required, null statement */
 
-    while (getchar() != EOF) 
-        ++nc;
-
-    printf("%ld\n",nc);
+    printf("%.0f\n",nc);
 }
