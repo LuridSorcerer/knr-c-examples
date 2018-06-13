dcacb06837e05a53cbde8ca0b5186d7069a48314
Andrew Lingenfelter <luridsorcerer@gmail.com>
2018-06-13 17:13:43 -0400

    changed magic numbers to #defines

--- chapter1/temp_for.c
+++ chapter1/temp_for.c
@@ -6,12 +6,16 @@
 /* standard I/O library */
 #include <stdio.h>
 
+#define LOWER 0    /* lower limit of table */
+#define UPPER 300  /* upper limit of table */
+#define STEP  20   /* step size */
+
 /* print Fahrenheit to Celsius table */
 main() {
 
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
         
 }
