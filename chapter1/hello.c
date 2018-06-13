fe4d8125e5e44f50f8366afded797265a2c1f170
Andrew Lingenfelter <luridsorcerer@gmail.com>
2018-06-13 15:23:13 -0400

    added comments to hello.c

--- chapter1/hello.c
+++ chapter1/hello.c
@@ -1,13 +1,15 @@
 /* hello.c 
  * 
- * The classic programming beginner's project.
- * 
+ * The classic programming beginner's project. 
  */
   
+/* Include the standard I/O library */
 #include <stdio.h>
 
+/* Create a function, main, that expects no arguments */
 main() {
 
+    /* print a message to the screen */
     printf("Hello, world.\n");
 
 }
