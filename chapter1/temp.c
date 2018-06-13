a2699a895f540e60ebda1c4c56e95bdaf10e2f89
Andrew Lingenfelter <luridsorcerer@gmail.com>
2018-06-13 17:00:45 -0400

    changed temperatures from int to float

--- chapter1/temp.c
+++ chapter1/temp.c
@@ -8,7 +8,7 @@
 
 main() 
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
@@ -17,8 +17,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%3d %6d\n",fahr,celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n",fahr,celsius);
         fahr = fahr + step;
     }
 }
