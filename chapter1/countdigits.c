215031dc42813cd7b1d08e41be1593bc42165c84
Andrew Lingenfelter <luridsorcerer@gmail.com>
2018-06-13 22:47:07 -0400

    Added countdigits.c

---
+++ chapter1/countdigits.c
@@ -0,0 +1,33 @@
+/* countdigits.c
+ * 
+ * Count occurrences of each digit in a given input.
+ */
+
+/* standard I/O library */
+#include <stdio.h>
+
+main() {
+    int c,i,nwhite,nother;
+    
+    /* an array, one member for each digit */
+    int ndigit[10];
+
+    nwhite = nother = 0;
+
+    for (i=0; i < 10; ++i) 
+        ndigit[i] = 0;
+    
+    while ( (c=getchar()) != EOF ) 
+        if (c >= '0' && c <= '9')
+            ++ndigit[c-'0'];
+        else if (c==' ' || c=='\n' || c== '\t')
+            ++nwhite;
+        else
+            ++nother;
+    
+    printf("digits = ");
+    for (int i = 0; i < 10; ++i) 
+        printf("%d",ndigit[i]);
+    printf(", white space = %d, other = %d\n", 
+        nwhite, nother);
+}
